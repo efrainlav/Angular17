f8a24aea70f52b80712e51f38362d75aa4027244
efrainlav <efrainlav@gmail.com>
2023-12-14 08:23:40 -0500

    Ejemplo de reactividad en angular usando Signals

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,13 +11,13 @@ import { CommonModule } from '@angular/common';
 export class LabsComponent {
   title = 'todoapp';
   tasks = ['Instalar el Angular CLI', 'Crear proyecto', 'Crear componentes'];
-  name = 'Efrain';
+  name = signal('Efrain');
   age = 40;
   disabled = false;
   img = 'https://w3schools.com/howto/img_avatar.png';
 
   person = {
-    name: 'Efrain',
+    name: signal('Efrain'),
     age: 40,
     disabled: false,
     avatar: 'https://w3schools.com/howto/img_avatar.png',
@@ -28,7 +28,9 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event) {
-    console.log(event);
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
   }
 
   keydownHandler(event: KeyboardEvent) {
